370b031757281d04237a6a72458f45ae7cd2ad30
Berdnyk-O <berdnyk.oleksandr.v@gmail.com>
2024-07-10 18:27:16 +0300

    add a background

---
+++ Assets/Scrips/Background.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Sin(Time.time*0.3f), Mathf.Cos(Time.time * 0.3f), 12.88f);  
+    }
+}
